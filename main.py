2ed652ab339e6ff67968588eeb6c7dd887b44b7e
psopi <59154239+Psopis@users.noreply.github.com>
2022-08-02 20:31:32 +0700

    3tr

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ class CategorySecond:
     def __init__(self, title, descrpition, list_category, list_object):
         self.title = title  # имя человека
         self.descrpition = descrpition
-        self.list_category = list_category
+        self.listhref = list_category
         self.list_object = list_object
 
     def print(self):
-        print(self.title, self.descrpition, self.list_category, self.list_object)
+        print(self.title, self.descrpition, self.listhref, self.list_object)
 
 
 class Category:
@@ -58,39 +58,6 @@ def parsing_category_list(url):
     return listhref
 
 
-def parsing_object(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'lxml')
-    title = soup.find("div", id='content').find('h1').text
-    admin_product = soup.find('div', class_='left').find('a').text
-    model = title.split(" ")[0]
-    description = soup.find('div', id='tab-description').find('ul').text.strip()
-    return title, admin_product, model, description
-
-
-def parsing_splash(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'lxml')
-    title = soup.find("div", id='content').find('h1').text
-    descrpition = soup.find('div', class_='category-info').find('ul').text
-    list = []
-    for a in soup.find('div', class_='category-list').findAll('a', href=True):
-        list.append(a['href'])
-    return list
-
-
-def parsing_splashsecond(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'lxml')
-    href = ""
-    for i in \
-            soup.find('div', class_="content").find('div', class_="product-list").find('div', class_="left").find('div',
-                                                                                                                  class_="name").find(
-                'a', href=True)['href']:
-        href = f"{i}"
-    parsing_object(i)
-
-
 fn = 'shop.xlsx'
 wb = load_workbook(fn)
 ws = wb['data']
@@ -104,10 +71,10 @@ urllist = ['http://www.shopprom.ru/aspiration/wamflo/wamflo_filtroelement/']
 for a in soup.find('div', class_='category-list').find('ul').findAll('a', href=True):
     urllist.append(a['href'])
 
-#     list = ""
-#     for i in parse.listhref:
-#         list = f"{i}, "
-#     ws.append([parse.title, parse.descrpition, list])
+
+def save_ws(title, model, description, creator):
+    ws.append([title, creator, model, description])
+
 
 listurlobj = []
 for i in urllist:
@@ -120,23 +87,90 @@ def parsing_category_category(url):
     soup = BeautifulSoup(r.text, 'lxml')
     title = soup.find('div', id='content').find('h1').text
     description = soup.find('div', class_='category-info').find('ul').text
-    list_category = soup.find('div', id='content').find('div', class_='category-list').findAll('a', href=True)
-    list_object = soup.find('#content > div.product-list > div:nth-child(1) > div.left > div.image > a')
+    list_category = []
+    for item in soup.find('div', id='content').find('div', class_='category-list').find_all('li'):
+        list_category.append(item.find('a', href=True)['href'])
+    list_object = []
+    for href in soup.find_all("#content > div.product-list > div:nth-child(1) > div.left"):
+        list_object.append(href.find('div', class_='name').find('a', href=True)['href'])
+
     return CategorySecond(title=title, descrpition=description, list_category=list_category, list_object=list_object)
 
 
-def parsing_category_obj(url):
+def parsing_category_category_first(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'lxml')
     title = soup.find('div', id='content').find('h1').text
-    creator = soup.find('#content > div.product-info > div.right > div.description > a').text
-    model = title.split(" ")[0]
-    description = soup.find("#tab-description > ul").text
-    return Category_object(title=title, creator=creator, model=model, description=description)
+    description = soup.find('div', class_='category-info').find('ul').text
+    list_category = []
+    for item in soup.find('div', id='content').find('div', class_='category-list').find_all('li'):
+        list_category.append(item.find('a', href=True)['href'])
+
+    return Category(title=title, descrpition=description, listhref=list_category)
 
 
-# parsing_category_category('http://www.shopprom.ru/aspiration/wamflo/wamflo_filtroelement/fnc_filtroelem/').print()
-# for i in parsing_category_category('http://www.shopprom.ru/aspiration/wamflo/wamflo_filtroelement/fnc_filtroelem/'):
-#     print(i)
-# wb.save(fn)
-# wb.close()
+def take_href_from_splash(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'lxml')
+    list = []
+    for item in soup.find('div', id="content").find('div', class_="product-list").find_all('div', class_='name'):
+        list.append(item.find('a', href=True).get('href'))
+    return list
+
+
+def parsing_category_obj_in_one_example(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'lxml')
+    title = soup.find('div', id='content').find('h1').text
+    creator = soup.find('div', class_='right').find('a').text
+    model = soup.find('/html/body/div[1]/div[5]/div[2]/div[2]/div[1]/text()[1]')
+    description = soup.find('div', id='content').find('div', id='tab-description').text.strip()
+
+    return Category_object(title=title, creator=creator, model=model, descrpition=description)
+
+
+def add_in_csv_obj(clas):
+    save_ws(clas.title, clas.creator, clas.model, clas.descrpition)
+
+
+def add_in_csv_category(clas):
+    list = ""
+    for uri in clas.listhref:
+        list = f"{uri}, "
+    ws.append([clas.title, clas.descrpition, list])
+
+
+urls = [
+        'http://www.shopprom.ru/aspiration/wamflo/wamflo_filtroelement/fnc_filtroelem/',
+        'http://www.shopprom.ru/aspiration/wamflo/wamflo_filtroelement/kfnm_series/',
+        'http://www.shopprom.ru/aspiration/wamflo/wamflo_filtroelement/kfew_series/']
+
+first = parsing_category('http://www.shopprom.ru/aspiration/wamflo/wamflo_filtroelement/')
+add_in_csv_category(first)
+second = parsing_category_category_first('http://www.shopprom.ru/aspiration/wamflo/wamflo_filtroelement/kfne_series/')
+add_in_csv_category(second)
+
+for i in second.listhref:
+    for item in take_href_from_splash(i):
+        print(item)
+        parsing_category_obj_in_one_example(item).print()
+        add_in_csv_obj(parsing_category_obj_in_one_example(item))
+    pass
+
+for uri in urls:
+    category = parsing_category_category(uri)
+    add_in_csv_category(category)
+    category.print()
+    for item in category.listhref:
+
+        for i in take_href_from_splash(item):
+            obj = parsing_category_obj_in_one_example(i)
+            parsing_category_obj_in_one_example(i).print()
+            add_in_csv_obj(obj)
+    for item in category.list_object:
+        obj = parsing_category_obj_in_one_example(item)
+        add_in_csv_obj(obj)
+        obj.print()
+
+wb.save(fn)
+wb.close()
